1c90cb64b1e44a83e49bff236a2296933bfc0225
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2024-11-17 13:05:40 +0600

    .

--- src/Component/NewStore.jsx
+++ src/Component/NewStore.jsx
@@ -7,7 +7,7 @@ const NewStore = () => {
   return (
     <div>
         <Container className='w-[340px] md:w-full py-[50px] md:py-[70px] '>
-                 <div className='w-[340px] md:w-[245px] flex md:flex-col  md:items-start items-center'>
+                 <div className='w-[340px] md:w-[245px] flex md:flex-col justify-between  md:items-start items-center'>
                   <h1 className='font-Saira w-[124px] text-[24px] md:text-[40px] font-semibold md:w-[219px]'>New In Store Now</h1>
                   <p className='font-Saira w-[206px] md:w-[245px] text-[14px] md:text-[16px] mt-0 md:mt-[89px] mb-0  md:mb-12 '>Get the latest items immediately with promo prices</p>
 
