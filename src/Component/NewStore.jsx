8e59a23ba1b4f6e1f6ae7ff5db3845104b226ceb
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2024-11-17 11:15:48 +0600

    .

--- src/Component/NewStore.jsx
+++ src/Component/NewStore.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Container from './Layer/Container'
+import { FaArrowLeft } from 'react-icons/fa'
+import { FaArrowRightLong } from 'react-icons/fa6'
 
 const NewStore = () => {
   return (
     <div>
-        <Container className='w-[340px] md:w-full '>
-                 NewStore
+        <Container className='w-[340px] md:w-full py-[50px] md:py-[70px] '>
+                 <div className='w-[340px] md:w-[245px] flex md:flex-col  md:items-start items-center bg-slate-300 '>
+                  <h1 className='font-Saira w-[124px] text-[24px] md:text-[40px] font-semibold md:w-[219px]'>New In Store Now</h1>
+                  <p className='font-Saira w-[206px] md:w-[245px] text-[14px] md:text-[16px] mt-0 md:mt-[89px] mb-0  md:mb-12 '>Get the latest items immediately with promo prices</p>
+
+                  <div className='md:flex items-center hidden font-Saira gap-2.5 '>
+                  <button className='underline'>Check All 
+                  </button>
+                  <FaArrowRightLong /> 
+                  </div>
+                 </div>
         </Container>
     </div>
   )
